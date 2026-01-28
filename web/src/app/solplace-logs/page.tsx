ee209372a4923b91af9c71f2e70bb6fe893d482f
sub1n17 <sub1n17@naver.com>
2026-01-29 08:09:01 +0900

    fix: 빌드 오류 수정

--- web/src/app/solplace-logs/page.tsx
+++ web/src/app/solplace-logs/page.tsx
@@ -1,5 +1,5 @@
 import SolPlaceList from '@/src/components/domains/solplace-list';
 
 export default function solPlaceListPage() {
-    return <SolPlaceList isPlace={true}></SolPlaceList>;
+    return <SolPlaceList></SolPlaceList>;
 }
